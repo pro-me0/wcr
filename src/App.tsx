7f9b1a33121433f81a137927f67a962c67166dbb
soomtochukwu <onwuajuesesomtochukwu@gmail..com>
2024-02-20 02:10:43 +0100

    maintanance

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,22 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { useState } from "react";
 import "./App.css";
-
 function App() {
   const [contact, setContact] = useState("");
-  /*  _setContact = async (arg: string) => {
-      console.log(`a: ${arg}`);
-      console.log(`con: ${contact}`);
-
-      let _number: string;
-      const args = arg.split(" ");
-
-      args.forEach((item) => {
-        _number = _number += item;
-      });
-
-      // @ts-ignore
-      setContact(_number);
-      // console.log(_number);
-    }; */
-
   return (
     <>
-      <h1>Welcome </h1>
-      <h2>Enter your friend's WhatsApp number below </h2>
-
+      <h1>Welcome</h1>
+      <h2>Enter your friend's WhatsApp number below to dive into their DMs</h2>
       <form action={"https://wa.me/" + contact} target="_bl" method="get">
         <input
           type="number"
           inputMode="numeric"
           placeholder="Enter friends's number"
-          // value={contact}
           onChange={(e) => setContact(e.target.value)}
         />
-        <input type="submit" value="Enter Dm" />
+        <input type="submit" value="Enter their Dm" />
       </form>
     </>
   );
 }
-
 export default App;
