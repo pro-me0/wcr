4044fa76a9e2f0ccbefe33fe76ab6766f84d4835
pro-me0 <secondarymazi@gmail.com>
2024-02-24 19:20:40 +0100

    added all country codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { useState } from "react";
 import "./App.css";
+import { countryCodes } from "./utils/countryCodes";
+
 function App() {
-  const
-  [contact, setContact] = useState("")
+  const [contact, setContact] = useState(""),
+    [countryCode, setCountryCode] = useState("");
   // [val, setVal] = useState("")
-  ;
   return (
     <>
       <h1>Welcome</h1>
       <h2>Enter your friend's WhatsApp number below to dive into their DMs</h2>
-      <form action={"https://wa.me/234" + contact} target="_bl" method="get">
+      <form
+        action={`https://wa.me/${countryCode + contact}`}
+        target="_bl"
+        method="get"
+      >
+        {/* <input className="input" type="text" value={"+234"} readOnly className="ro"/> */}
 
-        <input type="text" value={"+234"} readOnly className="ro"/>
+        <select
+          className="countryCode"
+          onChange={(e) => {
+            setCountryCode(e.target.value);
+          }}
+        >
+          {countryCodes.map((country) => {
+            return country.name == "Nigeria" ? (
+              <option
+                aria-placeholder={country.dial_code}
+                selected
+                value={country.dial_code}
+              >
+                {country.name}
+              </option>
+            ) : (
+              <option value={country.dial_code}>{country.name}</option>
+            );
+          })}
+        </select>
 
         <input
-        autoFocus
+          className="number"
+          autoFocus
           type="number"
           inputMode="numeric"
           placeholder="Enter friends's number"
           // value={val}
           onChange={(e) => {
-            const value = e.target.value
-            setContact(value);
+            let value = e.target.value;
 
-            if(value.startsWith("0")){
-              // setVal("")
-              alert("Your first input cannot be '0'")
-              e.target.value = ""
-            }else{
+            if (value.startsWith("0")) {
+              // e.target.value = "";
+              value = value.replace("0", "");
+              setContact(value);
+            } else {
               /* const formattedNumber = value.replace(value.charAt(2), value.charAt(2) + " ");
               
               // formattedNumber = value.replace(value.charAt(6), value.charAt(6) + " ");
@@ -38,7 +63,7 @@ function App() {
             }
           }}
         />
-        <input type="submit" value="Enter their Dm" />
+        <input className="enter" type="submit" value="Enter their Dm" />
       </form>
     </>
   );
